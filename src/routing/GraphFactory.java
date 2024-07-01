bda351d2d74d52094782771b9848022559a40992
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-07-01 21:54:27 +0200

    Create GraphFactory with default implementations

---
+++ src/routing/GraphFactory.java
@@ -0,0 +1,69 @@
+package routing;
+
+import java.util.Iterator;
+
+public class GraphFactory implements Graph {
+
+    @Override
+    public Node getNode(long id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Coordinate getNWCoordinate() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Coordinate getSECoordinate() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<Node> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int numEdges() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int numNodes() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int removeIsolatedNodes() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int removeUntraversableEdges(RoutingAlgorithm ra, TravelType tt) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isOverlayGraph() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isOverlayGraph'");
+    }
+
+    @Override
+    public Node getNodeInUnderlyingGraph(long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNodeInUnderlyingGraph'");
+    }
+
+    
+    
+}
