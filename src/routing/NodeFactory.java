eb9cfa2e4c7df644d3239e99e8118a53d8fbbe1d
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-07-01 23:06:27 +0200

    Implemented NodeFactory Class methods using ArrayList

---
+++ src/routing/NodeFactory.java
@@ -0,0 +1,54 @@
+package routing;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class NodeFactory implements Node{
+
+    public long id;
+    public double lat;
+    public double lon;
+    public ArrayList<Edge> edges = new ArrayList<Edge>();
+
+    public NodeFactory(long id, double lat, double lon) {
+        this.id = id;
+        this.lat = lat;
+        this.lon = lon;
+    }
+    
+    @Override
+    public Coordinate getCoordinate() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Edge getEdge(int idx) {
+        return this.edges.get(idx);
+    }
+
+    @Override
+    public long getId() {
+        return this.id;
+    }
+
+    @Override
+    public Iterator<Edge> iterator() {
+        return this.edges.iterator();
+    }
+
+    @Override
+    public int numEdges() {
+        return this.edges.size();
+    }
+
+    @Override
+    public void addEdge(Edge e) {
+        this.edges.add(e);   
+    }
+
+    @Override
+    public void removeEdge(int i) {
+        this.edges.remove(i);
+    }
+}
