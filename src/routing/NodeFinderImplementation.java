eacb35e638ab152545e97678a6dff702b827dfaf
paritoshparashar <papa00001@stud.uni-saarland.de>
2024-07-04 17:21:59 +0200

    Task 3 Completed, Not Checked

--- src/routing/NodeFinderImplementation.java
+++ src/routing/NodeFinderImplementation.java
@@ -1,5 +1,7 @@
 package routing;
 
+import static org.junit.Assert.assertTrue;
+
 import java.util.ArrayList;
 
 public class NodeFinderImplementation implements NodeFinder {
@@ -18,15 +20,14 @@ public class NodeFinderImplementation implements NodeFinder {
     @Override
     public Node getNodeForCoordinates(Coordinate c) {
 
-        
+        long trueClosestNodeId = 0; 
+        double trueClosestNodeDistance = Double.MAX_VALUE;
         double candidateNodeDistance = 0.0;
-        
+
+
          // Find the cell in the grid that contains the coordinate
             int numRows = this.gridObj.gridRows();
             int numCols = this.gridObj.gridColumns();
-
-            double cellWidth = this.gridObj.cellWidth();
-            double cellHeight = this.gridObj.cellHeight();
 
             int row = this.gridObj.getRow(c);
             int col = this.gridObj.getCol(c);
@@ -37,7 +38,8 @@ public class NodeFinderImplementation implements NodeFinder {
 
             if (nodesInCell != null) {
                 // Candidate Node is in the same cell
-                candidateNodeDistance = this.calculateClosestNodeDistanceInAGridCell(c, nodesInCell);    
+                long candidateNodeId = this.calculateClosestNodeInAGridCell(c, nodesInCell);  
+                candidateNodeDistance = this.graph.getNode(candidateNodeId).getCoordinate().getDistance(c);   
             }
             else {
                 // Get the nodes in the neighbouring cells
@@ -51,12 +53,15 @@ public class NodeFinderImplementation implements NodeFinder {
 
                     for (int i = -(searchAreaLat); i <= (searchAreaLat); i++) {
                         for (int j = -(searchAreaLong); j <= (searchAreaLong); j++) {
-                            if (row + i >= 0 && row + i < numRows && col + j >= 0 && col + j < numCols) {
-                                if (this.gridObj.getGrid()[row + i][col + j] != null) {
-                                    nodeListInNeighbouringCell.addAll(this.gridObj.getGrid()[row + i][col + j]);
+
+                            if ((row + i >= 0 && row + i < numRows && col + j >= 0 && col + j < numCols)
+                                    && this.gridObj.getGrid()[row + i][col + j] != null) {
+                                
+
+                                    assertTrue(nodeListInNeighbouringCell.addAll(this.gridObj.getGrid()[row + i][col + j]));
                                     found = true;
                                     break;
-                                }
+                                
                             }
                         }
                         if (found) {
@@ -71,32 +76,79 @@ public class NodeFinderImplementation implements NodeFinder {
                     
                 
                 // Candidate Node is in the neighbouring cell [row+i, col+j]
-                candidateNodeDistance = this.calculateClosestNodeDistanceInAGridCell(c, nodeListInNeighbouringCell);
+                long candidateNodeId = this.calculateClosestNodeInAGridCell(c, nodeListInNeighbouringCell);
+                candidateNodeDistance = this.graph.getNode(candidateNodeId).getCoordinate().getDistance(c);
             }
 
-            // Now we have the candidate node, we need create the boundary box around the candidate node
-                // to calculate the true closest node
+            /*
+             * Now we have the candidate node, we need to create the boundary box around the candidate node
+             * to calculate the true closest node
+            */
+            
                 double radius = candidateNodeDistance;
-                CoordinateBox boundaryBox = c.computeBoundingBox(candidateNodeDistance);
+                CoordinateBox boundaryBox = c.computeBoundingBox(radius);
+
+                // Get the bounds of grid cells that intersect with the boundary box
+                Coordinate topLeft = new Coordinate(boundaryBox.getUpperBound().getLatitude() , boundaryBox.getUpperBound().getLongitude());
+                Coordinate bottomRight = new Coordinate(boundaryBox.getLowerBound().getLatitude() , boundaryBox.getLowerBound().getLongitude());
+
+                int topLeftRow = this.gridObj.getRow(topLeft);
+                int topLeftCol = this.gridObj.getCol(topLeft);
+
+                int bottomRightRow = this.gridObj.getRow(bottomRight);
+                int bottomRightCol = this.gridObj.getCol(bottomRight);
+
                 
+                // Get the closest node, by comparing the closest node of each cell in the boundary box
+                ArrayList<Long> nodesInBoundaryCells = new ArrayList<>();
+
+
+                for (int i = topLeftRow; i <= bottomRightRow; i++) {
+                    for (int j = topLeftCol; j <= bottomRightCol; j++) {
+
+                        double currentMinDistance = 0.0;
+
+                        if ((i >= 0 && i < numRows && j >= 0 && j < numCols)
+                                    && (this.gridObj.getGrid()[i][j] != null)) {
+                            
+                                assertTrue(nodesInBoundaryCells.addAll(this.gridObj.getGrid()[i][j]));
+
+                                long closestNodeId = calculateClosestNodeInAGridCell(c, nodesInBoundaryCells);
+                                currentMinDistance = this.graph.getNode(closestNodeId).getCoordinate().getDistance(c);
+
+                                if (currentMinDistance < trueClosestNodeDistance) {
+                                    trueClosestNodeId = closestNodeId;
+                                    trueClosestNodeDistance = currentMinDistance;
+                                }
+
+                        }
+
+                    }
+                }
+
+                // Now we have the true closest node
+                return this.graph.getNode(trueClosestNodeId);
+
 
     }
 
-    public double calculateClosestNodeDistanceInAGridCell (Coordinate c, ArrayList<Long> nodesInCell) {
+    public long calculateClosestNodeInAGridCell (Coordinate c, ArrayList<Long> nodesInCell) {
 
         double minDistanceCandidateNode = Double.MAX_VALUE;
+        Node closestNode = null;
 
-        for (Long nodeId : nodesInCell) {
+        for (long nodeId : nodesInCell) {
 
             Node currentNode = this.graph.getNode(nodeId);
             double distance = currentNode.getCoordinate().getDistance(c);
 
             if (distance < minDistanceCandidateNode) {
+                closestNode = currentNode;
                 minDistanceCandidateNode = distance;
             }
         }
 
-        return minDistanceCandidateNode;
+        return closestNode.getId();
     }
 
 
